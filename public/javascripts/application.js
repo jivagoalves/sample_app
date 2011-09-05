06ffa7835e43e51610a11496fe9abb3386015374
Jivago Alves <jivagoalves@gmail.com>
2011-09-05 14:42:57 -0400

    Chapter 11 - exercises

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function(){
+jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
+
+jQuery.fn.submitWithAjax = function() {
+  this.submit(function() {
+    $.post(this.action, $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
+
+$(document).ready(function() {
+  $("#new_micropost").submitWithAjax();
+
   $(".countdown").simplyCountable({
     counter: '.content-counter',
     strictMax: true
   });
-});
+
+  $(".microposts tbody tr form").submitWithAjax();
+})
